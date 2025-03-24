1456642f8ad9ce7faffefc3f267b55eedc2afbbb
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-03-24 07:10:46 -0400

    Add some vim options

--- init.lua
+++ init.lua
@@ -157,6 +157,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.o.autoindent = true
+vim.o.expandtab = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
