f12311f9660508c3fa2ca0ef79d5b3119bda1d53
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-03-24 07:13:23 -0400

    Disable treesitter highlighting for json

--- init.lua
+++ init.lua
@@ -932,6 +932,9 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        disable = {
+          'json',
+        },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
